514d4ae6b98b41e98a64084c9723d16d0441b781
AmpatzidisSavvas <savvas.ampa@gmail.com>
2024-02-16 19:18:23 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     setUserInput((prevInputUser) => {
       return {
         ...prevInputUser,
-        [inputIdentifier]: newValue,
+        [inputIdentifier]: +newValue, 
       };
     });
   }
